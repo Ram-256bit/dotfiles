9f66db21b143d72f6bf2d799a773397188b0dca7
Ram-256bit <72127084+Ram-256bit@users.noreply.github.com>
2025-04-28 11:54:53 +0530

    nvim - disabled mason, added keymap for Tab to go to next buffer

--- nvim/.config/nvim/lua/plugins/example.lua
+++ nvim/.config/nvim/lua/plugins/example.lua
@@ -181,19 +181,24 @@ return {
 
   -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
   { import = "lazyvim.plugins.extras.lang.json" },
-
-  -- add any tools you want to have installed below
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "stylua",
-        "shellcheck",
-        "shfmt",
-        "flake8",
-      },
-    },
-  },
+  --
+  -- -- add any tools you want to have installed below
+  -- {
+  --   "williamboman/mason.nvim",
+  --   enabled = false,
+  --   -- opts = {
+  --   --   ensure_installed = {
+  --   --     "stylua",
+  --   --     "shellcheck",
+  --   --     "shfmt",
+  --   --     "flake8",
+  --   --   },
+  --   -- },
+  -- },
+  -- { "williamboman/mason-lspconfig.nvim",
+  --   enabled = false 
+  -- },
+  --
   {
     "christoomey/vim-tmux-navigator",
     cmd = {

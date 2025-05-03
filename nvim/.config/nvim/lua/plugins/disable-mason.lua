12d366dee679f34c9fae6e49db00e4a3893f5043
Ram-256bit <72127084+Ram-256bit@users.noreply.github.com>
2025-05-03 19:09:41 +0530

    nvim - disable markdown-preview, add supermaven, json lsp

--- nvim/.config/nvim/lua/plugins/disable-mason.lua
+++ nvim/.config/nvim/lua/plugins/disable-mason.lua
@@ -1,4 +1,6 @@
 return {
   { "williamboman/mason-lspconfig.nvim", enabled = false },
   { "williamboman/mason.nvim", enabled = false },
+
+  -- { "iamcco/markdown-preview.nvim", enabled = false },
 }
